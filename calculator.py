3e59bc880227b23619134e83eb967bd116c4c070
RisheeChandra <bookflightfiji@gmail.com>
2024-09-21 21:57:28 +1200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+x = input ("pick a number, it can be any number. ")
+
+y = input ("Now pick a different number. ")
+
+z = int (x) + int (y) 
+print("If you add those numbers you get",z )
+
+a = input ("""pretty cool huh?
+This time pick another number, any number """)
+
+b = input("Got it? alright pick a different number now ")
+
+c = input ("Now pick a different number than before")
+
+Answer = int(a) - int(b) - int(c)
+
+print ("If you substract all of them together you'll get",Answer )
+
+print("Neat huh? But I bet you could have done it as well sooo")
+ 
